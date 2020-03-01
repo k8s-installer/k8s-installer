0b99a523621683d9da67590948b1fe0d2b491106
Takuya Murakami <tmurakam@tmurakam.org>
2020-03-01 15:14:42 +0900

    add audit dependency to resolve conflict

--- rhel7/setup-offline.sh
+++ rhel7/setup-offline.sh
@@ -15,14 +15,14 @@ sudo yum install -y yum-utils createrepo
 # download rpms
 echo "==> Downloading rpms"
 sudo mkdir -p $K8S_OFFLINE_YUM_REPO
-sudo repotrack -a x86_64 -p $K8S_OFFLINE_YUM_REPO docker kubeadm kubectl kubelet
+sudo repotrack -a x86_64 -p $K8S_OFFLINE_YUM_REPO docker kubeadm kubectl kubelet audit
 sudo /bin/rm $K8S_OFFLINE_YUM_REPO/*.i686.rpm
 sudo createrepo $K8S_OFFLINE_YUM_REPO
 
 # install & start docker
 echo "==> Install and start docker"
 sudo cp kubernetes-offline.repo /etc/yum.repos.d/
-sudo yum install -y --disablerepo=\\* --enablerepo=kubernetes-offline docker
+sudo yum install -y --disablerepo="*" --enablerepo=kubernetes-offline docker
 sudo systemctl enable docker
 sudo systemctl start docker
 
