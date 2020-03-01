25b93833e555174ab163c62db4f28e53ba443a4c
Takuya Murakami <tmurakam@tmurakam.org>
2020-03-01 14:12:19 +0900

    add test-kubeadm-init.sh, etc

--- rhel7/setup-offline.sh
+++ rhel7/setup-offline.sh
@@ -1,28 +1,23 @@
 #!/bin/sh
 
-if [ `id -u` -ne 0 ]; then
-    echo "Error: You must run this with sudo."
-    exit 1
-fi
-
 # setup repo
-subscription-manager repos --enable=rhel-7-server-extras-rpms
-cp kubernetes.repo /etc/yum.repos.d/
+sudo subscription-manager repos --enable=rhel-7-server-extras-rpms
+sudo cp kubernetes.repo /etc/yum.repos.d/
 
-yum check-update -y
+sudo yum check-update -y
 
 # download docker stuff
 ./download-rpms.sh pkglist-docker.txt
 
 # install & start docker
-yum install -y files/rpms/*.rpm
-systemctl enable docker
-systemctl start docker
+sudo yum install -y files/rpms/*.rpm
+sudo systemctl enable docker
+sudo systemctl start docker
 
 # download kubernetes stuff
-./download-rpms.sh pkglist-kubeadm.txt
+sudo ./download-rpms.sh pkglist-kubeadm.txt
 
 # download kubernetes container images
-./pull-images.sh
+sudo ./pull-images.sh
 
 
---
+++ rhel7/test-kubeadm-init.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+sudo swapoff -a
+
+sudo yum install -y files/rpms/*.rpm
+sudo systemctl enable --now kubelet
+
+sudo setenforce 0
+
+sudo cat <<EOF > /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+sudo sysctl --system
+
+sudo kubeadm init
+
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
