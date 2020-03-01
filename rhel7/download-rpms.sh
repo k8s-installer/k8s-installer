3be54c7924bfb45bc5630a279478aab191ff39c7
Takuya Murakami <tmurakam@tmurakam.org>
2020-03-01 13:48:29 +0900

    improve

--- rhel7/download-rpms.sh
+++ rhel7/download-rpms.sh
@@ -14,7 +14,7 @@ PKGITEMS=`cat $PKGLIST | sed "s/#.*$//g" | sort -u `
 echo $PKGITEMS
 
 #sudo yum reinstall --downloadonly --downloaddir=./rpms $PKGITEMS
-yumdownloader -x \*i686 --archlist=x86_64 --destdir ./files/rpms $PKGITEMS
+yumdownloader --resolve -x \*i686 --archlist=x86_64 --destdir ./files/rpms $PKGITEMS
 
 #/bin/rm container-selinux-1*.rpm
 
--- rhel7/setup-offline.sh
+++ rhel7/setup-offline.sh
@@ -1,20 +1,26 @@
 #!/bin/sh
 
-mkdir -p files/images
-mkdir -p files/rpms
+if [ `id -u` -ne 0 ]; then
+    echo "Error: You must run this with sudo."
+    exit 1
+fi
 
+# setup repo
+subscription-manager repos --enable=rhel-7-server-extras-rpms
 cp kubernetes.repo /etc/yum.repos.d/
 
+# download docker stuff
 ./download-rpms.sh pkglist-docker.txt
-./download-rpms.sh pkglist-kubeadm.txt
 
-subscription-manager repos --enable=rhel-7-server-extras-rpms
-
+# install & start docker
 yum install files/rpms/*.rpm
-
 systemctl enable docker
 systemctl start docker
 
+# download kubernetes stuff
+./download-rpms.sh pkglist-kubeadm.txt
+
+# download kubernetes container images
 ./pull-images.sh
 
 
