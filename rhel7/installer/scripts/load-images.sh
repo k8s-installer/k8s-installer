64faef5ced8d10ae8a51faacee5b74e3d2fca90a
Takuya Murakami <tmurakam@tmurakam.org>
2020-04-03 22:36:07 +0900

    update: load-images

--- rhel7/installer/scripts/load-images.sh
+++ rhel7/installer/scripts/load-images.sh
@@ -5,11 +5,11 @@
 echo "==> Extracting container images"
 tar xvzf kubernetes-images.tar.gz
 
-IMGLIST=images/images.txt
-IMAGES=`cat $IMGLIST | sed "s/#.*$//g" | sort -u `
-echo $IMAGES
+#IMGLIST=images/images.txt
+#IMAGES=`cat $IMGLIST | sed "s/#.*$//g" | sort -u `
+#echo $IMAGES
 
-for i in $IMAGES; do
+for i in images/*.tar; do
     echo "==> Loading $i"
-    docker load -i images/`basename $i`.tar
+    docker load -i $i
 done
