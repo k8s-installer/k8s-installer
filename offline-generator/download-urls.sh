57c9176d1c02509d4c4e200464a78aaa735dfb05
Takuya Murakami <tmurakam@tmurakam.org>
2020-04-23 18:17:34 +0900

    offline-generator: add metrics-server.yaml

--- offline-generator/download-urls.sh
+++ offline-generator/download-urls.sh
@@ -5,7 +5,7 @@ download() {
   file="$2"
 
   if [ ! -e "offline-files/$file" ]; then
-    echo "Downloading: $url"
+    echo "===> Downloading: $url"
     curl -SL "$url" > "offline-files/$file" || exit 1
   fi
 }
@@ -14,9 +14,10 @@ CALICO_VERSION=v3.13
 METRICS_SERVER_VERSION=v0.3.6
 HELM_VERSION=v3.1.2
 
+# download yamls
 download https://docs.projectcalico.org/${CALICO_VERSION}/manifests/calico.yaml calico.yaml
 download https://github.com/kubernetes-sigs/metrics-server/releases/download/${METRICS_SERVER_VERSION}/components.yaml metrics-server.yaml
 
+# download helm
 HELM_FILE=helm-${HELM_VERSION}-linux-amd64.tar.gz
 download https://get.helm.sh/$HELM_FILE $HELM_FILE
-
