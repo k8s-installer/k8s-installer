ed98d3f5dfd5729cba083ae8ffdb1cba8a94a9fc
Takuya Murakami <tmurakam@tmurakam.org>
2020-04-26 19:25:16 +0900

    update: offline-generator: add nfs etc (ubuntu)

--- offline-generator/scripts/create-repo-ubuntu.sh
+++ offline-generator/scripts/create-repo-ubuntu.sh
@@ -3,6 +3,17 @@
 #. ./check-root.sh
 . ./config.sh
 
+# Package list
+DOCKER_PKGS="apt-transport-https ca-certificates curl gnupg-agent software-properties-common"
+DOCKER_PKGS="docker.io $DOCKER_PKGS"
+#DOCKER_PKGS="docker-ce docker-ce-cli containerd.io firewalld python-cryptography $DOCKER_PKGS"
+
+PKGS="$DOCKER_PKGS firewalld nfs-common nfs-kernel-server"
+
+for v in $KUBE_VERSIONS; do
+    PKGS="$PKGS kubeadm=${v}-00 kubelet=${v}-00 kubectl=${v}-00"
+done
+
 # setup k8s repo
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
@@ -15,30 +26,17 @@ EOF
 #deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable
 #EOF
 
-# install tools
-DOCKER_DEPS="apt-transport-https ca-certificates curl gnupg-agent software-properties-common"
-sudo apt-get update && sudo apt-get install -y $DOCKER_DEPS
+# install docker
+sudo apt-get update && sudo apt-get install -y $DOCKER_PKGS
 
 CACHEDIR=outputs/cache-debs
 mkdir -p $CACHEDIR
-
-cp ./config.sh outputs
-
-#DEPS="docker-ce docker-ce-cli containerd.io firewalld python-cryptography"
-DEPS="docker.io firewalld"
-
-PKGLIST="$DOCKER_DEPS $DEPS"
-
-for v in $KUBE_VERSIONS; do
-    PKGLIST="$PKGLIST kubeadm=${v}-00 kubelet=${v}-00 kubectl=${v}-00"
-done
-
 
 # Get all dependencies
 DEPS=$(apt-cache depends --recurse --no-recommends --no-suggests --no-conflicts --no-breaks --no-replaces --no-enhances --no-pre-depends $PKGLIST | grep "^\w" | sort | uniq)
 
 # Download packages
-(cd $CACHEDIR && apt download $PKGLIST $DEPS)
+(cd $CACHEDIR && apt download $PKGS $DEPS)
 
 # Create repo
 DEBDIR=outputs/debs
@@ -55,6 +53,9 @@ apt-ftparchive packages . > Packages && gzip -c9 Packages > Packages.gz
 apt-ftparchive contents . > Contents-amd64 && gzip -c9 Contents-amd64 > Contents-amd64.gz
 apt-ftparchive release . > Release
 popd
+
+# Copy config.sh
+cp ./config.sh outputs
 
 # Create tarball
 (cd outputs && mkdir -p offline-files && tar cvzf offline-files/k8s-offline-apt-repo.tar.gz debs config.sh)
