3d1e3dc1ff153c3a9f767a793a362718987f4b5f
Takuya Murakami <tmurakam@tmurakam.org>
2020-05-06 00:34:30 +0900

    add: offline-generator: add containerd, runc

--- offline-generator/scripts/download-urls.sh
+++ offline-generator/scripts/download-urls.sh
@@ -12,6 +12,10 @@ download() {
   url="$1"
   file="$2"
 
+  if [ -z "$file" ]; then
+      file=$(basename $url)
+  fi
+
   if [ ! -e "$OUTDIR/$file" ]; then
     echo "===> Downloading: $url"
     curl -SL "$url" > "$OUTDIR/$file" || exit 1
@@ -23,11 +27,20 @@ download() {
 CALICO_VERSION=v3.13
 METRICS_SERVER_VERSION=v0.3.6
 HELM_VERSION=v3.1.2
+CONTAINERD_VERSION=1.3.4
 
 # download yamls
-download https://docs.projectcalico.org/${CALICO_VERSION}/manifests/calico.yaml calico.yaml
+download https://docs.projectcalico.org/${CALICO_VERSION}/manifests/calico.yaml
 download https://github.com/kubernetes-sigs/metrics-server/releases/download/${METRICS_SERVER_VERSION}/components.yaml metrics-server.yaml
 
 # download helm
 HELM_FILE=helm-${HELM_VERSION}-linux-amd64.tar.gz
-download https://get.helm.sh/$HELM_FILE $HELM_FILE
+download https://get.helm.sh/$HELM_FILE
+
+# download runC
+RUNC_VERSION=v1.0.0-rc10
+download https://github.com/opencontainers/runc/releases/download/${RUNC_VERSION}/runc.amd64 runc.amd64_${RUNC_VERSION}
+
+# download containerd
+CONTAINERD_FILE=containerd-${CONTAINERD_VERSION}.linux-amd64.tar.gz
+download https://github.com/containerd/containerd/releases/download/v${CONTAINERD_VERSION}/${CONTAINERD_FILE}
