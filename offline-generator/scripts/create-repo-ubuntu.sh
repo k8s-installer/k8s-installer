25bd3d3bf22a7118158eaa2d96afa56e12ad1ba2
Takuya Murakami <tmurakam@tmurakam.org>
2020-05-09 10:48:13 +0900

    update: offline-repo: ubuntu repo

--- offline-generator/scripts/create-repo-ubuntu.sh
+++ offline-generator/scripts/create-repo-ubuntu.sh
@@ -16,6 +16,7 @@ for v in $KUBE_VERSIONS; do
 done
 
 # setup k8s repo
+echo "===> Setup k8s repo"
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
 deb https://apt.kubernetes.io/ kubernetes-xenial main
@@ -28,30 +29,35 @@ EOF
 #EOF
 
 # install docker
+echo "===> Installing docker"
 sudo apt-get update && sudo apt-get install -y $DOCKER_PKGS
 
 CACHEDIR=outputs/cache-debs
 mkdir -p $CACHEDIR
 
 # Resolve all dependencies
+echo "===> Resolving dependencies"
 DEPS=$(apt-cache depends --recurse --no-recommends --no-suggests --no-conflicts --no-breaks --no-replaces --no-enhances --no-pre-depends $PKGS | grep "^\w" | sort | uniq)
+#DEPS=$(apt-cache depends --recurse $PKGS | grep "^\w" | sort | uniq)
+#echo "$DEPS"
 
 # Download packages
+echo "===> Downloading packages"
 (cd $CACHEDIR && apt download $PKGS $DEPS)
 
 # Create repo
+echo "===> Creating repo"
 DEBDIR=outputs/debs
 if [ -e $DEBDIR ]; then
     /bin/rm -rf $DEBDIR
 fi
-mkdir $DEBDIR
-/bin/cp $CACHEDIR/* $DEBDIR
-/bin/rm $DEBDIR/*i386.deb
+mkdir -p $DEBDIR/packages
+/bin/cp $CACHEDIR/* $DEBDIR/packages
+/bin/rm $DEBDIR/packages/*i386.deb
 
 pushd $DEBDIR || exit 1
-apt-ftparchive sources . > Sources && gzip -c9 Sources > Sources.gz
-apt-ftparchive packages . > Packages && gzip -c9 Packages > Packages.gz
-apt-ftparchive contents . > Contents-amd64 && gzip -c9 Contents-amd64 > Contents-amd64.gz
+apt-ftparchive packages . | gzip -c9 > Packages.gz
+apt-ftparchive contents . | gzip -c9 > Contents-amd64.gz
 apt-ftparchive release . > Release
 popd
 
@@ -59,5 +65,8 @@ popd
 cp ./config.sh outputs
 
 # Create tarball
-(cd outputs && mkdir -p offline-files && tar cvzf offline-files/k8s-offline-apt-repo.tar.gz debs config.sh)
+(cd outputs && mkdir -p offline-files && tar czf offline-files/k8s-offline-apt-repo.tar.gz debs config.sh)
+
+echo "Done."
+
 
