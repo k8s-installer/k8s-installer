e18037f45e4924b25ef2fd7df5c06b2e8d24cf0d
Takuya Murakami <tmurakam@tmurakam.org>
2020-09-01 23:22:02 +0900

    [UPDATE] update calico 3.15.1 -> 3.16.0

--- ChangeLog.md
+++ ChangeLog.md
@@ -1,5 +1,10 @@
 # ChangeLog
 
+## 1.18.8-1 - 2020/09/01
+
+- Update rook 1.4.0 -> 1.4.2
+- Update calico 3.15.1 -> 3.16.0
+
 ## 1.18.8-0 - 2020/08/26
 
 Version number scheme is changed.
--- ansible/roles/network-plugin/calico/download.sh
+++ ansible/roles/network-plugin/calico/download.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 #VER=v3.13
 #VER=v3.14
-VER=v3.15
+#VER=v3.15
+VER=v3.16
 
 curl -SLO https://docs.projectcalico.org/$VER/manifests/calico.yaml
 curl -SLO https://docs.projectcalico.org/$VER/manifests/calico-typha.yaml
