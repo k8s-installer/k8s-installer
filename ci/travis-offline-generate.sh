d24fa70e94ff239c8e1b73dbe8c7a5e0cca96da1
Takuya Murakami <tmurakam@tmurakam.org>
2020-09-24 14:19:10 +0900

    update travis.yml: add more OSs

--- ci/travis-offline-generate.sh
+++ ci/travis-offline-generate.sh
@@ -5,7 +5,7 @@ PJDIR=$PWD
 echo "Starting docker"
 docker run -d -it -v $HOME:$HOME $BASE /bin/bash
 
-DOCKER_CONTAINER_ID=$(docker ps | grep centos | awk '{print $1}')
+DOCKER_CONTAINER_ID=$(docker ps | grep "$BASE" | awk '{print $1}')
 echo "Container id is $DOCKER_CONTAINER_ID please wait while updates applied"
 
 docker exec -it $DOCKER_CONTAINER_ID /bin/bash -xec "cd $PJDIR/offline-generator && ./generate-offline.sh"
