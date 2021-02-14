7b3270c4027ca93879f0b7584ccdac9963dcb911
Takuya Murakami <tmurakam@tmurakam.org>
2021-02-14 21:22:22 +0900

    feat: Use --upload-certs

--- ansible/roles/master/get-join-files/files/get-join-files.sh
+++ ansible/roles/master/get-join-files/files/get-join-files.sh
@@ -8,24 +8,27 @@ fi
 TMPDIR=$(mktemp -d)
 cd "$TMPDIR" || exit 1
 
+# Upload certificates and get certificate key
+kubeadm init phase --config /etc/kubernetes/kubeadm-config.yml upload-certs --upload-certs | tail -1 > certificateKey || exit 1
+
 # Generate token and get join command
 kubeadm token create --print-join-command --kubeconfig=/etc/kubernetes/admin.conf > join.sh || exit 1
 
 sed -i  's/^\(kubeadm join.*\)$/\1 $*/' join.sh
 chmod 755 *.sh
 
-mkdir -p pki/etcd
+#mkdir -p pki/etcd
 
-cp \
-     /etc/kubernetes/pki/ca* \
-     /etc/kubernetes/pki/sa* \
-     /etc/kubernetes/pki/front-proxy-ca* \
-     ./pki/
-cp /etc/kubernetes/pki/etcd/ca* ./pki/etcd
-cp /etc/kubernetes/admin.conf .
+#cp \
+#     /etc/kubernetes/pki/ca* \
+#     /etc/kubernetes/pki/sa* \
+#     /etc/kubernetes/pki/front-proxy-ca* \
+#     ./pki/
+#cp /etc/kubernetes/pki/etcd/ca* ./pki/etcd
+#cp /etc/kubernetes/admin.conf .
 
-tar cvzf /tmp/join-files.tar.gz .
-cp join.sh /tmp/
+#tar cvzf /tmp/join-files.tar.gz .
+cp join.sh certificateKey /tmp/
 
 cd /
 /bin/rm -rf "$TMPDIR"
