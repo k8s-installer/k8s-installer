aac0f20b8b1bcf3f7ecded8cd756a57a18b1a862
Takuya Murakami <tmurakam@tmurakam.org>
2021-02-15 00:10:29 +0900

    fix: Add --kubelet-insecure-tls args to metrics-server

--- ansible/roles/apps/metrics-server/download.sh
+++ ansible/roles/apps/metrics-server/download.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
+
+# Note: need to add '--kubelet-insecure-tls' args.
 curl -L https://github.com/kubernetes-sigs/metrics-server/releases/download/v0.4.2/components.yaml > files/components.yaml
