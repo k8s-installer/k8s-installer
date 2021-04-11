5accff105231a4adfa9ad0b831a3614dbe8a6e55
Takuya Murakami <tmurakam@tmurakam.org>
2021-04-11 22:14:25 +0900

    chore: Fix install-docker.sh (offline)

--- offline-generator/scripts/install-docker.sh
+++ offline-generator/scripts/install-docker.sh
@@ -7,7 +7,7 @@ if ! type docker >/dev/null 2>&1; then
     if [ -e /etc/redhat-release ]; then
         $SUDO yum install -y docker || (echo "Error: can't install docker" && exit 1)
     else
-        $SUDO apt install docker.io || (echo "Error: can't install docker.io" && exit 1)
+        $SUDO apt install -y docker.io || (echo "Error: can't install docker.io" && exit 1)
     fi
 fi
 $SUDO systemctl enable --now docker
